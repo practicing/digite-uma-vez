cca2ce16a6e17708753581ebd8376482daa1b98c
Luis Felipe Souza <luisfmsouza@gmail.com>
2015-10-10 13:46:31 -0300

    Issue #3: adding JS uglify

--- Gulpfile.js
+++ Gulpfile.js
@@ -26,50 +26,70 @@ gulp.task('browser-sync', function() {
     //gulp.watch("source/html/*.html").on('change', browserSync.reload);
 });
 
+function onError(error) {
+    console.log('# plumber #');
+    console.log(error);
+}
+
 
 // Sass
 gulp.task('css:compress', function () {
     return gulp.src(paths.css.input)
-  	.pipe(plugins.sourcemaps.init())
-    .pipe(plugins.sass({
-    		outputStyle: 'compressed'
-    	}).on('error', plugins.sass.logError)
-    )
-    .pipe(plugins.sourcemaps.write('./'))
-    .pipe(gulp.dest(paths.css.output))
-    .pipe(browserSync.stream());
+        .pipe(plugins.plumber({errorHandler: onError}))
+      	.pipe(plugins.sourcemaps.init())
+        .pipe(plugins.sass({
+        		outputStyle: 'compressed'
+        	}).on('error', plugins.sass.logError)
+        )
+        .pipe(plugins.sourcemaps.write('./'))
+        .pipe(gulp.dest(paths.css.output))
+        .pipe(browserSync.stream());
 });
 
 // Scss Lint
 gulp.task('css:lint', function() {
 	return gulp.src(paths.css.input)
-	//.pipe( plugins.plumber({errorHandler: onError}) )
-	.pipe(plugins.scssLint({
-		endless: true,
-		sync: true
-	}))
-	.pipe(plugins.scssLint.failReporter());
+    	.pipe(plugins.plumber({errorHandler: onError}))
+    	.pipe(plugins.scssLint({
+    		endless: true,
+    		sync: true
+    	}))
+    	.pipe(plugins.scssLint.failReporter());
 });
 
 // JS Uglify
 gulp.task('js:compress', function() {
-    gulp.src(paths.js.input)
-    .pipe(plugins.sourcemaps.init())
-    .pipe(plugins.concat('digite-uma-vez.js'))
-    .pipe(plugins.rename({
-        suffix: '.min'
-    }))
-    .pipe(plugins.uglify())
-    .pipe(plugins.sourcemaps.write('./'))
-    .pipe(gulp.dest(paths.js.output));
+    return gulp.src(paths.js.input)
+        .pipe(plugins.plumber({errorHandler: onError}))
+        .pipe(plugins.sourcemaps.init())
+        .pipe(plugins.concat('digite-uma-vez.js'))
+        .pipe(plugins.rename({
+            suffix: '.min'
+        }))
+        .pipe(plugins.uglify())
+        .pipe(plugins.sourcemaps.write('./'))
+        .pipe(gulp.dest(paths.js.output));
 });
 
+
+// JS Eslint
+gulp.task('js:lint', function () {
+    return gulp.src([paths.js.input])
+        .pipe(plugins.plumber({errorHandler: onError}))
+        .pipe(plugins.eslint())
+        .pipe(plugins.eslint.format())
+        // .pipe(plugins.eslint.failAfterError());
+});
+
+gulp.task('js:watch', ['js:lint'], function () {
+    gulp.watch([paths.js.input], ['js:lint']);
+})
 
 
 // Build the code
 
 // Browser Sync helper tasks
-gulp.task('js:bsWatch', ['js:compress'], browserSync.reload);
+gulp.task('js:bsWatch', ['js:lint', 'js:compress'], browserSync.reload);
 gulp.task('css:bsWatch', ['css:lint', 'css:compress'], browserSync.stream);
 
 // Main tasks
