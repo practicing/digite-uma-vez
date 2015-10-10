1f335188fd67395186e1e227f4d0416ebd67d1ee
Luis Felipe Souza <luisfmsouza@gmail.com>
2015-10-10 12:31:30 -0300

    Issue #3: adding JS uglify

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,47 +1,48 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
-var sass = require('gulp-sass');
-var sourcemaps = require('gulp-sourcemaps');
-var scssLint = require('gulp-scss-lint');
-
 var plugins = require('gulp-load-plugins')();
 
 var paths = {
     css: {
-    	inp: './source/sass/*.scss',
-    	out: './css'
+    	input: './source/sass/*.scss',
+    	output: './css'
+    },
+    js: {
+        input: './source/js/*.js',
+        output: './js'
     }
 }
 
 // Static server
-gulp.task('default', function() {
+gulp.task('browser-sync', function() {
     browserSync.init({
         server: {
             baseDir: "./"
         }
     });
 
-    gulp.watch(paths.css.inp, ['styleLint', 'style']);
+    gulp.watch(paths.css.input, ['css:bsWatch']);
+    gulp.watch(paths.js.input, ['js:bsWatch']);
     //gulp.watch("source/html/*.html").on('change', browserSync.reload);
 });
 
 
 // Sass
-gulp.task('style', function () {
-    return gulp.src(paths.css.inp)
-  	.pipe(sourcemaps.init())
-    .pipe(sass({
+gulp.task('css:compress', function () {
+    return gulp.src(paths.css.input)
+  	.pipe(plugins.sourcemaps.init())
+    .pipe(plugins.sass({
     		outputStyle: 'compressed'
-    	}).on('error', sass.logError)
+    	}).on('error', plugins.sass.logError)
     )
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest(paths.css.out))
+    .pipe(plugins.sourcemaps.write('./'))
+    .pipe(gulp.dest(paths.css.output))
     .pipe(browserSync.stream());
 });
 
-
-gulp.task('styleLint', function() {
-	return gulp.src(paths.css.inp)
+// Scss Lint
+gulp.task('css:lint', function() {
+	return gulp.src(paths.css.input)
 	//.pipe( plugins.plumber({errorHandler: onError}) )
 	.pipe(plugins.scssLint({
 		endless: true,
@@ -50,6 +51,24 @@ gulp.task('styleLint', function() {
 	.pipe(plugins.scssLint.failReporter());
 });
 
+// JS Uglify
+gulp.task('js:compress', function() {
+    gulp.src(paths.js.input)
+    .pipe(plugins.sourcemaps.init())
+    .pipe(plugins.concat('digite-uma-vez.js'))
+    .pipe(plugins.rename({
+        suffix: '.min'
+    }))
+    .pipe(plugins.uglify())
+    .pipe(plugins.sourcemaps.write('./'))
+    .pipe(gulp.dest(paths.js.output));
+});
+
+
 
 // Build the code
+gulp.task('js:bsWatch', ['js:compress'], browserSync.reload);
+gulp.task('css:bsWatch', ['css:lint', 'css:compress'], browserSync.stream);
+// gulp.task('js:compress', ['js-compress']);
+gulp.task('server', ['browser-sync']);
 gulp.task('build', ['styleLint']);
