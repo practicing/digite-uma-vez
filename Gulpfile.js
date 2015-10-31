b149e1e2f479fcd3036ac2da9d0bf223a1a65a93
Luis Felipe Souza <luisfmsouza@gmail.com>
2015-10-31 17:24:44 -0200

    Including ESlint on SublimeText and fixing ESLint errors on Gulpfile.js

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,96 +1,99 @@
+'use strict';
+
 var gulp        = require('gulp');
 var browserSync = require('browser-sync').create();
 var plugins = require('gulp-load-plugins')();
 
 var paths = {
-    css: {
-    	input: './source/sass/*.scss',
-    	output: './css'
-    },
-    js: {
-        input: './source/js/*.js',
-        output: './js'
-    }
-}
+  css: {
+    input: './source/sass/*.scss',
+    output: './css'
+  },
+  js: {
+    input: './source/js/*.js',
+    output: './js'
+  }
+};
 
 // Static server
-gulp.task('browser-sync', function() {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    });
+gulp.task('browser-sync', function () {
+  browserSync.init({
+    server: {
+      baseDir: './'
+    }
+  });
 
-    gulp.watch(paths.css.input, ['css:bsWatch']);
-    gulp.watch(paths.js.input, ['js:bsWatch']);
-    gulp.watch('*.html').on('change', browserSync.reload);
+  gulp.watch(paths.css.input, ['css:bsWatch']);
+  gulp.watch(paths.js.input, ['js:bsWatch']);
+  gulp.watch('*.html').on('change', browserSync.reload);
 });
 
-function onError(error) {
-    console.log('# plumber #');
-    console.log(error);
+function onError (error) {
+  console.log('# plumber #');
+  console.log(error);
 }
 
 
 // Sass
 gulp.task('css:compress', function () {
-    return gulp.src(paths.css.input)
-        .pipe(plugins.plumber({errorHandler: onError}))
-      	.pipe(plugins.sourcemaps.init())
-        .pipe(plugins.sass({
-        		outputStyle: 'compressed'
-        	}).on('error', plugins.sass.logError)
-        )
-        .pipe(plugins.sourcemaps.write('./'))
-        .pipe(gulp.dest(paths.css.output))
-        .pipe(browserSync.stream());
+  return gulp.src(paths.css.input)
+    .pipe(plugins.plumber({errorHandler: onError}))
+    .pipe(plugins.sourcemaps.init())
+    .pipe(
+      plugins.sass({
+        outputStyle: 'compressed'
+      }).on('error', plugins.sass.logError)
+    )
+    .pipe(plugins.sourcemaps.write('./'))
+    .pipe(gulp.dest(paths.css.output))
+    .pipe(browserSync.stream());
 });
 
 // Scss Lint
-gulp.task('css:lint', function() {
-	return gulp.src([paths.css.input])
-    	// .pipe(plugins.plumber({errorHandler: onError}))
-    	.pipe(plugins.scssLint({
-    		endless: true,
-    		sync: true
-    	}))
-    	.pipe(plugins.scssLint.failReporter());
+gulp.task('css:lint', function () {
+  return gulp.src([paths.css.input])
+      // .pipe(plugins.plumber({errorHandler: onError}))
+      .pipe(plugins.scssLint({
+        endless: true,
+        sync: true
+      }))
+      .pipe(plugins.scssLint.failReporter());
 });
 
 // JS Uglify
-gulp.task('js:compress', function() {
-    return gulp.src(paths.js.input)
-        .pipe(plugins.plumber({errorHandler: onError}))
-        .pipe(plugins.sourcemaps.init())
-        .pipe(plugins.concat('digite-uma-vez.js'))
-        .pipe(plugins.rename({
-            suffix: '.min'
-        }))
-        .pipe(plugins.uglify({
-          // mangle: false,
-          // compress: false
-        }))
-        .pipe(plugins.sourcemaps.write('./'))
-        .pipe(gulp.dest(paths.js.output));
+gulp.task('js:compress', function () {
+  return gulp.src(paths.js.input)
+    .pipe(plugins.plumber({errorHandler: onError}))
+    .pipe(plugins.sourcemaps.init())
+    .pipe(plugins.concat('digite-uma-vez.js'))
+    .pipe(plugins.rename({
+      suffix: '.min'
+    }))
+    .pipe(plugins.uglify({
+      // mangle: false,
+      // compress: false
+    }))
+    .pipe(plugins.sourcemaps.write('./'))
+    .pipe(gulp.dest(paths.js.output));
 });
 
 
 // JS Eslint
 gulp.task('js:lint', function () {
-    return gulp.src([paths.js.input])
-        // .pipe(plugins.plumber({errorHandler: onError}))
-        .pipe(plugins.eslint())
-        .pipe(plugins.eslint.format())
-        .pipe(plugins.eslint.failAfterError());
+  return gulp.src([paths.js.input])
+    // .pipe(plugins.plumber({errorHandler: onError}))
+    .pipe(plugins.eslint())
+    .pipe(plugins.eslint.format())
+    .pipe(plugins.eslint.failAfterError());
 });
 
 gulp.task('js:watchLint', ['js:lint'], function () {
-    gulp.watch([paths.js.input], ['js:lint']);
+  gulp.watch([paths.js.input], ['js:lint']);
 });
 
 
 gulp.task('css:watchLint', ['css:lint'], function () {
-    gulp.watch([paths.css.input], ['css:lint']);
+  gulp.watch([paths.css.input], ['css:lint']);
 });
 
 
