15f14a5eb7ee785df1f12df692a7ce872bcaa7e3
Luis Felipe Souza <luisfmsouza@gmail.com>
2015-10-31 19:06:39 -0200

    Fixing eslint issues on server.js file

--- server.js
+++ server.js
@@ -1,25 +1,27 @@
-var http = require("http");
-var fs = require("fs");
-var path = require("path");
-var mime = require("mime");
+'use strict';
 
-function send404(response) {
-  response.writeHead(404, {"Content-type" : "text/plain"});
-  response.write("Error 404: resource not found");
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
+var mime = require('mime');
+
+function send404 (response) {
+  response.writeHead(404, {'Content-type': 'text/plain'});
+  response.write('Error 404: resource not found');
   response.end();
 }
 
-function sendPage(response, filePath, fileContents) {
-  response.writeHead(200, {"Content-type" : mime.lookup(path.basename(filePath))});
+function sendPage (response, filePath, fileContents) {
+  response.writeHead(200, {'Content-type': mime.lookup(path.basename(filePath))});
   response.end(fileContents);
 }
 
-function serverWorking(response, absPath) {
-  fs.exists(absPath, function(exists) {
+function serverWorking (response, absPath) {
+  fs.exists(absPath, function (exists) {
     if (exists) {
-      fs.readFile(absPath, function(err, data) {
+      fs.readFile(absPath, function (err, data) {
         if (err) {
-          send404(response)
+          send404(response);
         } else {
           sendPage(response, absPath, data);
         }
@@ -30,17 +32,19 @@ function serverWorking(response, absPath) {
   });
 }
 
-var server = http.createServer(function(request, response) {
+var server = http.createServer(function (request, response) {
   var filePath = false;
 
-  if (request.url == '/') {
-    filePath = "/index.html";
+  if (request.url === '/') {
+    filePath = '/index.html';
   } else {
-    filePath = "/" + request.url;
+    filePath = '/' + request.url;
   }
 
-  var absPath = "./" + filePath;
+  var absPath = './' + filePath;
   serverWorking(response, absPath);
 });
 
+/* eslint-disable camelcase, no-unused-vars */
 var port_number = server.listen(process.env.PORT || 3000);
+/* eslint-enable camelcase, no-unused-vars */
